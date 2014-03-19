50d647bb5f78ac71303c692c4fd6ecc299fd195c
Joseph Frazier <joseph@onsip.com>
2014-03-19 16:23:31 -0400

    Add barebones/ demo.

---
+++ barebones/phone.js
@@ -0,0 +1,151 @@
+// FILL IN THESE VALUES
+var config = {
+  wsServers: null,
+  uri: null,
+  authorizationUser: null,
+  password: null,
+  // FILL IN THOSE VALUES ^
+
+  userAgentString: 'SIP.js/0.5.0-devel BAREBONES DEMO',
+  traceSip: true,
+};
+
+var $ = document.getElementById.bind(document);
+
+// ensure config values are provided
+var requiredParams = ['wsServers', 'uri', 'authorizationUser', 'password'];
+requiredParams.some(function checkParam (param) {
+  if (config[param]) {
+    return false;
+  }
+
+  alert('config.' + param + ' is not set! Please open phone.js and set each of the following:\n\n\t* config.' + requiredParams.join('\n\t* config.'));
+  return true;
+});
+
+var ua = new SIP.UA(config);
+
+ua.on('invite', handleInvite);
+ua.on('message', receiveMessage);
+
+function handleInvite (s) {
+  var text = s.remoteIdentity.uri.toString() + ' is calling you. Accept?';
+  var accept = confirm(text);
+  if (accept) {
+    s.accept(getSessionOptions());
+    setupSession(s);
+  }
+  else {
+    s.reject();
+  }
+}
+
+function receiveMessage (e) {
+  var remoteUri = e.remoteIdentity.uri.toString();
+  showMessage(remoteUri, e.body);
+}
+
+function showMessage (from, body) {
+  $('chat-log').textContent += from + ': ' + body + '\n'
+}
+
+function sendMessage () {
+  if (!$('target').value) {
+    return;
+  }
+
+  var body = $('message').value;
+  ua.message($('target').value, body);
+  showMessage(ua.configuration.uri.toString(), body);
+}
+
+function sendDtmf () {
+  if (!session) {
+    return;
+  }
+
+  var tones = $('dtmf').value;
+  $('dtmf').value = '';
+
+  if (/[^1234567890#*]/.test(tones)) {
+    $('dtmf').placeholder = 'only 1234567890#*';
+  }
+  else {
+    $('dtmf').placeholder = 'Enter DTMF symbols';
+    session.dtmf(tones);
+  }
+}
+
+var session;
+
+function getSessionOptions () {
+  return {
+    media: {
+      audio: true,
+      video: $('video-enabled').checked
+    }
+  };
+}
+
+function dial () {
+  if (!$('target').value) {
+    return;
+  }
+
+  setupSession( ua.invite($('target').value, getSessionOptions()) );
+}
+
+function hangup () {
+  if (session) {
+    session.terminate();
+  }
+}
+
+function setupSession (s) {
+  hangup();
+  session = s;
+
+  session.on('accepted', onAccepted.bind(session));
+  session.once('bye', onTerminated.bind(session));
+  session.once('failed', onTerminated.bind(session));
+  session.once('cancel', onTerminated.bind(session));
+}
+
+function onTerminated () {
+  session = null;
+  attachMediaStream($('remote-media'), null);
+}
+
+function onAccepted () {
+  this.mediaHandler.getRemoteStreams().forEach(
+    attachMediaStream.bind(null, $('remote-media'))
+  );
+}
+
+function attachMediaStream (element, stream) {
+  if (typeof element.src !== 'undefined') {
+    URL.revokeObjectURL(element.src);
+    element.src = URL.createObjectURL(stream);
+  } else if (typeof element.srcObject !== 'undefined'
+       || typeof element.mozSrcObject !== 'undefined') {
+    element.srcObject = element.mozSrcObject = stream;
+  } else {
+    console.log('Error attaching stream to element.');
+    return false;
+  }
+
+  ensureMediaPlaying(element);
+  return true;
+}
+
+function ensureMediaPlaying (mediaElement) {
+  var interval = 100;
+  mediaElement.ensurePlayingIntervalId = setInterval(function () {
+    if (mediaElement.paused) {
+      mediaElement.play()
+    }
+    else {
+      clearInterval(mediaElement.ensurePlayingIntervalId);
+    }
+  }, interval);
+}
